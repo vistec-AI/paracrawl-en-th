4839ed7804226819f0be8b5d1d4719be67478b1e
Saiko Yoneyabashi <artificialcc@gmail.com>
2019-11-19 10:54:38 +0700

    Init project

---
+++ load_dataset.sh
@@ -0,0 +1,11 @@
+
+mkdir -p ./data
+
+echo "Downloading paracrawl v5 en-de."
+# curl https://s3.amazonaws.com/web-language-models/paracrawl/release5/en-de.classify.gz  > ./data/en-de.gz 
+curl https://s3.amazonaws.com/web-language-models/paracrawl/release5/en-de.bicleaner07.tmx.gz > ./data/en-de.bicleaner07.tmx.gz
+curl https://s3.amazonaws.com/web-language-models/paracrawl/release5/en-de.bicleaner07.txt.gz > ./data/en-de.bicleaner07.txt.gz 
+
+echo "Extracting paracrawl v5 en-de."
+tar -zxvf ./data/en-de.bicleaner07.tmx.gz
+tar -zxvf ./data/en-de.bicleaner07.txt.gz
