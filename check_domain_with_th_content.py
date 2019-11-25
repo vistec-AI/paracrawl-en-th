94cba36ddb9e1e5b54d4124da9903f9058c974b1
Saiko Yoneyabashi <artificialcc@gmail.com>
2019-11-25 11:30:58 +0700

    Add user agent

--- check_domain_with_th_content.py
+++ check_domain_with_th_content.py
@@ -101,17 +101,18 @@ def get_status(url):
     url_correct = url
     
     s = requests.Session()
+    ua = UserAgent()
     try:
         if 'http://' not in url:
             url_http = 'http://'  + url
             try: 
-                r = requests_retry_session(session=s).head(url_http)
+                r = requests_retry_session(session=s).head(url_http, headers={'User-Agent': ua.random})
                 url_correct = url_http
             except:
                         
                 try:
                     url_https = 'https://'  + url
-                    r = requests_retry_session(session=s).head(url_https)
+                    r = requests_retry_session(session=s).head(url_https, headers={'User-Agent': ua.random})
                     url_correct = url_https
 
                 except:
